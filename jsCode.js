20fad7c2014a54d445a31cfa434c15c885a00ca8
Hrishikesh Deshpande <you@example.com>
2024-10-05 13:39:31 +0530

    Complted

---
+++ jsCode.js
@@ -0,0 +1,40 @@
+
+// Positive Number Negative Number
+let number=10;
+
+if(number>0){
+ console.log("Number is positive : "+number);
+ 
+
+}
+else{
+    console.log("Number is Negative : "+number);
+    
+}
+
+function mult(number1) {
+ let n=0;
+ for(let i=1;i<=10;i++){
+   n+=number1;
+  console.log(""+n)
+ }    
+
+}
+mult(10);
+
+function cel(){
+
+
+
+}
+
+
+function cel(cel1) {
+        
+    let l1=(cel1*9/5)+32;
+    console.log(l1);
+    
+
+    
+}
+cel(3);
